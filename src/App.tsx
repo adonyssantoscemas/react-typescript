7c43bab78e6251fe3e940597d505108d9830a0dc
Adonys <adonyssantoscemas@gmail.com>
2021-02-20 15:09:07 -0400

    React + TypeScript: useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { Counter } from './components/Counter'
+import { Usuario } from './components/Usuario'
+
 function App() {
   return (
     <div className="App">
       <h1>React + TypeScript</h1>
+      <hr/ >
+		  <Counter />
+      <Usuario /> 
     </div>
   );
 }
